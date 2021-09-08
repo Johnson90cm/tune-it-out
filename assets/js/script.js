1ee29a74a6c542b5ce7e0b5654fe3a1073a1bb83
mmagera84 <86729483+mmagera84@users.noreply.github.com>
2021-09-08 12:57:54 -0600

    Update script.js
    function.artist.search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,14 @@
+
 clientId = '682d29adad28470bbaa8b701f8bbf44f'
 clientSecret = '635b54c1f31480290c2da839407b242'
 
-client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-var spotifyApi = SpotifyWebApi({
-  clientId: '682d29adad28470bbaa8b701f8bbf44f',
-  clientSecret: '6635b54c1f31480290c2da839407b242',
-  redirectUri: 'https://johnson90cm.github.io/group-project-1/callback',
-});
-
-/**
- * Get metadata of tracks, albums, artists, shows, and episodes
- */
-
- function getArtist(spotifyApi){
-  var apiUrl = "https://api.spotify.com/v1/artists/0oSGxfWSnnOXhD2fKuz2Gy" + 'clientId';
-
+function artistSearch() {
+  var searchName = document.querySelector(".searchName").value
+  console.log(searchName)
+  fetch('https://api.spotify.com/v1/artists//albums' + searchName +'/?app_id=29aaf39bfa83b4edff793b76ddddfba0')
+  .then(function (response) {
+      return response.json()
+    })
 // Get album
 spotifyApi.getAlbum('5U4W9E5WsYb2jUQWePT8Xm')
   .then(function(data) {
@@ -53,3 +45,46 @@ spotifyApi.getArtists(['2hazSY4Ef3aB9ATXW7F5w3', '6J6yx1t3nwIDyPXk5xa7O8'])
 spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
   .then(function(data) {
     console.log('Artist albums', data.body);
+  }, function(err) {
+    console.error(err);
+  });
+  // Get an artist's top tracks
+spotifyApi.getArtistTopTracks('0oSGxfWSnnOXhD2fKuz2Gy', 'GB')
+.then(function(data) {
+  console.log(data.body);
+  }, function(err) {
+  console.log('Something went wrong!', err);
+});
+
+// Get artists related to an artist
+spotifyApi.getArtistRelatedArtists('0qeei9KQnptjwb8MgkqEoy')
+.then(function(data) {
+  console.log(data.body);
+}, function(err) {
+  done(err);
+});
+
+/* Get Audio Features for a Track */
+spotifyApi.getAudioFeaturesForTrack('3Qm86XLflmIXVm1wcwkgDK')
+.then(function(data) {
+  console.log(data.body);
+}, function(err) {
+  done(err);
+});
+
+/* Get Audio Analysis for a Track */
+spotifyApi.getAudioAnalysisForTrack('3Qm86XLflmIXVm1wcwkgDK')
+.then(function(data) {
+  console.log(data.body);
+}, function(err) {
+  done(err);
+});
+play({
+  playerInstance: new Spotify.Player({ name: "..." }),
+  spotify_uri: 'spotify:track:7xGfFoTpQ2E7fRF5lN10tr',
+});
+window.onSpotifyWebPlaybackSDKReady = () => {
+  // You can now initialize Spotify.Player and use the SDK
+};
+spotifyApi.setAccessToken('BQDYbcsxkS7BgMrqK_7RMEstZCULyn5foqIJQdzZZNHxM0NTQJ-PvAiR8b2U2GO54Ple0Z-tZCrvbLGPwVDvKIpPioX-Ep1a_oZtxCWpJf2yczE3JWKTMArtkj_qx_Vn-v2M39XKMTElhp5pbdEqhpQihPamKxFcfw');
+}
